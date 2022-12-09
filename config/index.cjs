419dced2aabbd90fac670f26f29238a6be7818fc
a33456a <57582831+a33456a@users.noreply.github.com>
2022-12-09 16:38:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ USE_PASSAGE: 'push-deer',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU18156T6tr4uQ5QiHmzwqrVz2PJGaKTgwkjgGmG',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '001',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-13',
       festivals: [
